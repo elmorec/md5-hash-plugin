a58d34c95d48b6eff1531673957cba081031aaa2
Elmore Cheng <elmorec@users.noreply.github.com>
2018-08-03 11:27:44 +0800

    fix module source incorrect

--- plugin/md5-hash-plugin.js
+++ plugin/md5-hash-plugin.js
@@ -4,12 +4,8 @@ module.exports = class Md5HashPlugin {
   apply(compiler) {
     compiler.hooks.compilation.tap('chunk-hash', function (compilation) {
       compilation.hooks.chunkHash.tap("chunk-hash", function (chunk, chunkHash) {
-        const modules = Array.from(chunk.modulesIterable, modulesIterable => {
-          const _source = modulesIterable._source || {};
-          return _source._value ? _source._value.replace(/(?<=require\(['"]).*node_modules\//g, '') : '';
-        });
-        var source = modules.sort((a, b) => a.resource < b.resource ? -1 : 1).reduce((integral, i) => integral + i, '');
-        var chunk_hash = md5(source);
+        const source = getSource(chunk.entryModule._source ? chunk.entryModule : chunk.entryModule.modules);
+        const chunk_hash = md5(source);
         chunkHash.digest = function () {
           return chunk_hash;
         };
@@ -17,6 +13,14 @@ module.exports = class Md5HashPlugin {
     });
   }
 };
+
+function getSource(mod) {
+  if (!mod) return '';
+  if (Array.isArray(mod)) return mod.map(getSource).sort((a, b) => a.resource < b.resource ? -1 : 1).reduce((integral, i) => integral + i, '');
+
+  const _source = mod._source || {};
+  return _source._value ? _source._value.replace(/(?<=require\(['"]).*node_modules\//g, '') : '';
+}
 
 function md5(str, len = 20) {
   return crypto.createHash('md5').update(str).digest('hex').substring(0, len);
